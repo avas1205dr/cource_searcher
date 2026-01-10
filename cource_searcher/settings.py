cdac77496d8b549eda96d2151037d3b9890acc2c
pohomka <pohomycn@gmail.com>
2026-01-11 00:05:05 +0300

    add templates

--- cource_searcher/settings.py
+++ cource_searcher/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cource_searcher.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,13 +110,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
